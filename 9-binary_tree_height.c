e2375debb647a7093deef3a25e7a2d40b11c7ac7
madisonfleming <maddyyfleming@gmail.com>
2025-08-05 05:07:56 +0000

    file9check

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,19 +14,11 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	/* height of left and right tree */
-	else
-	{
-		left_height = binary_tree_height(tree->left);
-		right_height = binary_tree_height(tree->right);
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
 
-		if (left_height >= right_height)
-		{
-			return (left_height);
-		}
-		else
-		{
-			return (right_height + 1);
-		}
-	}
-}
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height ? left_height : right_height) + 1);
+}	
