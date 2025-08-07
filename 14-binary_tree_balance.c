aaa0c30d4df9729c28219307c1111ca5cd8252af
madisonfleming <maddyyfleming@gmail.com>
2025-08-07 07:25:46 +0000

    file14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,38 @@
 #include "binary_trees.h"
 #include <stdio.h>
-#include <stdlib>
+#include <stdlib.h>
 /**
+ * height - find the height of the tree
+ * @tree: pointer to the tree
+ * Return: height
  */
+int height(binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
 
+	if (tree == NULL)
+		return (0);
+
+	left_height = height(tree->left);
+	right_height = height(tree->right);
+
+return ((left_height > right_height ? left_height : right_height) + 1);
+}
+
+/**
+ * binary_tree_balance - check if the tree is balanced
+ * @tree: pointer to the tree
+ * Return: 1 if yes otherwise 0
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+       	if ((tree->left == NULL && tree->right != NULL) ||
+			(tree->left != NULL && tree->right == NULL))
+		return (-1);
+
+return (height(tree->left) - height(tree->right));
+}
