5eb0e606cfbeeca4799340faa2ce5df1c6afacea
madisonfleming <maddyyfleming@gmail.com>
2025-08-07 07:53:49 +0000

    fixbett

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -21,11 +21,12 @@ return (1 + (left > right ? left : right));
 }
 
 /**
- * is_perfect_recursion - travers the tree using recursion
+ * is_perfect - traverse the tree using recursion
  * @tree: pointer to tree
  * @d: depth of tree
+ * Return: value if perfect
  */
-int is_perfect_recursion(const binary_tree_t *tree, int d)
+int is_perfect(const binary_tree_t *tree, int d)
 {
 	if (tree == NULL)
 		return (0);
@@ -36,7 +37,7 @@ int is_perfect_recursion(const binary_tree_t *tree, int d)
 	if (tree->left == NULL || tree->right == NULL)
 	return (0);
 
-return (is_perfect_recursion(tree->left, d - 1) && is_perfect_recursion(tree->right, d - 1 ));
+return (is_perfect(tree->left, d - 1) && is_perfect(tree->right, d - 1));
 }
 
 /**
@@ -48,5 +49,5 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 {
 	int d = depth(tree);
 
-return (is_perfect_recursion(tree, d));
+return (is_perfect(tree, d));
 }
